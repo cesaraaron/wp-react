1052602395fd14656fad47eee53c7521c1c5b33b
Cesar Aaron <aaronash@live.com>
2017-10-23 16:52:16 -0600

    Refactor webpack config

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,13 +1,18 @@
 const config = require('./config')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   context: config.paths.src,
   entry: config.entry,
-  devtool: 'source-map',
   output: {
     path: config.paths.dist,
     filename: 'index.js'
   },
+  plugins: [
+    new CleanWebpackPlugin([config.paths.dist], {
+      root: config.paths.root
+    })
+  ],
   module: {
     rules: [
       {
