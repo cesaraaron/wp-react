7307b905e3dd2f7cdab494b05f12d3f37ef21b6b
Cesar Aaron <aaronash@live.com>
2017-12-03 18:11:24 -0600

    write some tests for Comments.js

---
+++ src/__tests__/Comments.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { Comment, CommentList } from '../Comments'
+import { comments } from '../data/SampleData'
+
+it('should render a comment', () => {
+  const component = renderer.create(<Comment {...comments[0]} />)
+
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('should render a list of comments', () => {
+  const component = renderer.create(<CommentList comments={comments} />)
+
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
