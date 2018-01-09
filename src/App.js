f54bc0e5cba44ca943b4dba9aa09909bd7967b1b
Cesar Aaron <aaronash@live.com>
2018-01-09 10:39:26 -0600

    add /search route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Posts from './Home'
 import Single from './Single'
 import Category from './Category'
+import Search from './Search'
 
 const App = () => (
   <div>
     <Route exact path="/" component={Posts} />
     <Route path="/page/:pageNumber" component={Posts} />
-    <Route exact path="/:slug" component={Single} />
     <Route path="/category/:slug" component={Category} />
+    <Switch>
+      <Route path="/search" component={Search} />
+      <Route exact path="/:slug" component={Single} />
+    </Switch>
   </div>
 )
 
