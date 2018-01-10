ef5101ed3f8c1ae96c0368a57d23d2a23e607d85
Cesar Aaron <aaronash@live.com>
2018-01-10 16:17:06 -0600

    create postsById reducer to cache all posts in it instead of having them scattered in createList

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,20 +13,42 @@ export const endpoint = (state = '', action) => {
   }
 }
 
+export const postsById = (state = {}, action) => {
+  switch (action.type) {
+    case types.FETCH_POSTS_SUCCESS:
+    case types.FETCH_SINGLE_SUCCESS:
+    case types.FETCH_POSTS_BY_SEARCH_QUERY_SUCCESS:
+    case types.FETCH_POSTS_BY_CATEGORY_SUCCESS:
+      return {
+        ...state,
+        ...action.response.entities.post
+      }
+    default:
+      return state
+  }
+}
+
 export default combineReducers({
+  endpoint,
+  postsById,
   [types.posts]: createList(types.posts),
   [types.single]: createList(types.single),
   [types.comments]: createList(types.comments),
   [types.postsByCategory]: createList(types.postsByCategory),
   [types.allCategories]: createList(types.allCategories),
-  [types.searchQuery]: createList(types.searchQuery),
-  endpoint
+  [types.searchQuery]: createList(types.searchQuery)
 })
 
 export const getTotalPages = (state, type) =>
   fromCreateList.getTotalPages(state[type])
 
 export const getEndpoint = state => state.endpoint
+
+export const getPosts = (state, type) =>
+  fromCreateList.getPosts(state[type], state.postsById)
+
+export const getPostsForPage = (state, type, pageNumber) =>
+  fromCreateList.getPostsForPage(state[type], pageNumber, state.postsById)
 
 export const getData = (state, type) => fromCreateList.getData(state[type])
 
