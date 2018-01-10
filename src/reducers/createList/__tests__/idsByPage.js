94ecb3ff03d8c4e3aa34b5bcbcdcf9582a49045e
Cesar Aaron <aaronash@live.com>
2018-01-10 16:14:22 -0600

    create idsByPage reducer

---
+++ src/reducers/createList/__tests__/idsByPage.js
@@ -0,0 +1,49 @@
+import { createIdsByPage, getPostsForPage } from '../idsByPage'
+import * as types from '../../../actions/types'
+import { normalize } from 'normalizr'
+import { arrayOfPosts } from '../../../actions/schema'
+import { posts } from '../../../utils/SampleData'
+
+const response = normalize(posts, arrayOfPosts)
+
+describe('createIdsByPage()', () => {
+  const idsByPage = createIdsByPage(types.posts)
+
+  it('should return an empty object', () => {
+    const actual = idsByPage(undefined, {})
+
+    expect(actual).toEqual({})
+  })
+
+  it('should return an object with keys pointing at arrays', () => {
+    const pageNumber = 1
+    const actual = idsByPage(undefined, {
+      type: types.FETCH_POSTS_SUCCESS,
+      response,
+      pageNumber
+    })
+
+    expect(actual).toEqual({ [pageNumber]: response.result })
+  })
+
+  it('should merge the state object with the action.response', () => {
+    const actual = idsByPage(
+      { 1: response.result },
+      { type: types.FETCH_POSTS_SUCCESS, pageNumber: 2, response }
+    )
+
+    expect(actual).toEqual({ 1: response.result, 2: response.result })
+  })
+})
+
+describe('getPostsForPage()', () => {
+  it('should get the posts given as params the pageNumber & byId', () => {
+    const pageNumber = 1
+    const postId = 1
+    const state = { [pageNumber]: [postId] }
+    const byId = { [postId]: { title: 'post title' } }
+    const actual = getPostsForPage(state, pageNumber, byId)
+
+    expect(actual).toEqual([{ title: 'post title' }])
+  })
+})
---
+++ src/reducers/createList/idsByPage.js
@@ -0,0 +1,27 @@
+import * as types from '../../actions/types'
+import invariant from 'invariant'
+
+const INVALID_PAGE_NUMBER = 'Invalid page number'
+
+export const createIdsByPage = type => (state = {}, action) => {
+  const { response, pageNumber } = action
+  const onFetch = types.createOnFetchVars(type)
+
+  switch (action.type) {
+    case onFetch.success:
+      return Number(pageNumber) > 0
+        ? {
+            ...state,
+            [pageNumber]: response.result
+          }
+        : state
+    default:
+      return state
+  }
+}
+
+export const getPostsForPage = (state, pageNumber, byId) => {
+  invariant(Number(pageNumber), INVALID_PAGE_NUMBER)
+
+  return state[pageNumber].map(id => byId[id])
+}
