a3d735b5ccef8f931ec71c2655b41777464edece
Cesar Aaron <aaronash@live.com>
2018-02-16 23:54:01 -0600

    remove react-hot-loader from index.js since create-react-app has it

--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import App from './App'
 import configureStore from './utils/configureStore'
 import { getEndpoint } from './actions'
-
-if (process.env.NODE_ENV !== 'production') {
-  const div = document.createElement('div')
-  div.setAttribute('id', 'root')
-  document.body.insertBefore(div, document.body.firstChild)
-}
+import './index.css'
+// import registerServiceWorker from './registerServiceWorker'
 
 const render = (Component, store) => {
   ReactDOM.render(
-    <AppContainer>
-      <Provider store={store}>
-        <Router>
-          <Component />
-        </Router>
-      </Provider>
-    </AppContainer>,
+    <Provider store={store}>
+      <Router>
+        <Component />
+      </Router>
+    </Provider>,
     document.getElementById('root')
   )
 }
@@ -31,9 +24,4 @@ const store = configureStore()
 store.dispatch(getEndpoint(window.location))
 
 render(App, store)
-
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    render(App)
-  })
-}
+// registerServiceWorker()
